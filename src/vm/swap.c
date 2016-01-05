92cadd23e199e3041bf1c2462100d3b901186306
Lukas Krämer <kraemer.luk@gmail.com>
2016-01-05 13:44:08 +0100

    added some comments

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -5,19 +5,23 @@ struct block swap_block;
 int swap_block_size;
 struct bitmap swap_map;
 
+
+// Init Swap at block device
 void init_swap() {
     swap_block = block_get_role(BLOCK_SWAP);
     swap_block_size = block_size(swap_block);
     swap_map = bitmap_create(swap_block_size);
 }
 
+// Create new swaptable entry from frametable entry
 struct swaptable_entry* 
-create_swaptable_entry(struct frame * f) {
+create_swaptable_entry(struct frametable_entry * f) {
     struct swaptable_entry* swap_entry = 0;//TODO: Where to allocate memory
     swap_entry -> frame = f;
     return swap_entry;
 } 
 
+// Returns next free swap address to store  
 block_sector_t
 swap_get_next_free() {
     int bitmap_size = bitmap_size(swap_map);
@@ -31,6 +35,7 @@ swap_get_next_free() {
     
 }
 
+// Writes a swaptable entry to memory
 void 
 swap_add(struct swaptable_entry * st_e) {
     //find free entry
@@ -42,12 +47,14 @@ swap_add(struct swaptable_entry * st_e) {
     st_e -> swap_sector = next_free_swap;
 }
 
+// Removes a swaptable entry
 void
 swap_remove(struct swaptable_entry * st_e) {
     int swap_page_size; //TODO
     bitmap_set_multiple(swap_map, st_e -> swap_sector, swap_page_size, false);
 }
 
+// Loads a swaptable entry to the given address
 void
 swap_read(struct swaptable_entry * st_e, void *addr) {
     // TODO Iterate over all sectors
