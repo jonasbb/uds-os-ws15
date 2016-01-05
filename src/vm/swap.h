ca8eb66a12d036c72e804d74ba469a3b37991b97
Lukas Krämer <kraemer.luk@gmail.com>
2016-01-05 13:38:12 +0100

    fixed function declarations

--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,17 +1,17 @@
-#include "vm/frame.h"
+#include "vm/frames.h"
 #include "devices/block.h"
 
 struct swaptable_entry {
     block_sector_t swap_sector;
     struct frametable_entry* frame; 
-}
+};
 
-void swap_init();
-struct swaptable_enrty* create_swaptable_entry(struct frame *);
+void init_swap(void);
+struct swaptable_entry* create_swaptable_entry(struct frametable_entry *);
 void swap_add(struct swaptable_entry *);
 void swap_read(struct swaptable_entry *, void *);
 void swap_remove(struct swaptable_entry *);
-block_sector_t swap_get_next_free();
+block_sector_t swap_get_next_free(void);
 
 
 
