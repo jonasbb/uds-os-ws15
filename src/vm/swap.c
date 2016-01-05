809b1b005b172111965f5fb99437820cdabfe015
Lukas Krämer <kraemer.luk@gmail.com>
2016-01-05 14:27:12 +0100

    added swap lock

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,15 +1,19 @@
 #include "vm/swap.h"
 #include "lib/kernel/bitmap.h"
+#include "lib/round.h"
+#include "threads/synch.h"
+#include "threads/vaddr.h"
 
 struct block swap_block;
 int swap_block_size;
 struct bitmap swap_map;
-
+struct lock swap_lock;
 
 // Init Swap at block device
 void init_swap() {
+    lock_init(swap_lock);    
     swap_block = block_get_role(BLOCK_SWAP);
-    swap_block_size = block_size(swap_block);
+    swap_block_size = DIV_ROUND_DOWN(block_size(swap_block), DIV_ROUND_UP(PGSIZE, BLOCK_SECTOR_SIZE) );
     swap_map = bitmap_create(swap_block_size);
 }
 
@@ -24,6 +28,7 @@ create_swaptable_entry(struct frametable_entry * f) {
 // Returns next free swap address to store  
 block_sector_t
 swap_get_next_free() {
+    lock_acquire(swap_lock);
     int bitmap_size = bitmap_size(swap_map);
     int swap_page_size; //TODO How big is a swap page?
     //TODO check if swap is full
@@ -31,6 +36,7 @@ swap_get_next_free() {
     if (next_free_swap != BITMAP_ERROR) {
         bitmap_set_multiple(swap_map, next_free_swap, swap_page_size, true);
     }
+    lock_release(swap_lock);
     return next_free_swap;
     
 }
@@ -40,9 +46,10 @@ void
 swap_add(struct swaptable_entry * st_e) {
     //find free entry
     block_sector_t next_free_swap = swap_get_next_free();
+    lock_acquire(swap_lock);
     // Iterate over all sectors
     block_write(swap_block, next_free_swap, st_e->frame->addr);
-    
+    lock_release(swap_lock);
     // Set addr in swaptable entry
     st_e -> swap_sector = next_free_swap;
 }
@@ -51,13 +58,17 @@ swap_add(struct swaptable_entry * st_e) {
 void
 swap_remove(struct swaptable_entry * st_e) {
     int swap_page_size; //TODO
+    lock_acquire(swap_lock);
     bitmap_set_multiple(swap_map, st_e -> swap_sector, swap_page_size, false);
+    lock_release(swap_lock);
 }
 
 // Loads a swaptable entry to the given address
 void
 swap_read(struct swaptable_entry * st_e, void *addr) {
+    lock_acquire(swap_lock);
     // TODO Iterate over all sectors
     block_read(swap_block, st_e ->swap_sector, addr);
-    swap_remove(st_e);
+    bitmap_set_multiple(swap_map, st_e -> swap_sector, swap_page_size, false);
+    lock_release(swap_lock);
 }
