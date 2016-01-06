87fc2a3f1d436eee6fe616ac084a3cd3e8f8a61d
Lukas Krämer <kraemer.luk@gmail.com>
2016-01-06 19:12:48 +0100

    clear frame pin at destroy

--- src/userprog/pagedir.c
+++ src/userprog/pagedir.c
@@ -41,8 +41,10 @@ pagedir_destroy (uint32_t *pd)
         uint32_t *pte;
         
         for (pte = pt; pte < pt + PGSIZE / sizeof *pte; pte++)
-          if (*pte & PTE_P) 
+          if (*pte & PTE_P) {
+            frame_set_pin(pte_get_page(*pte),false);
             palloc_free_page (pte_get_page (*pte));
+          }
         palloc_free_page (pt);
       }
   palloc_free_page (pd);
